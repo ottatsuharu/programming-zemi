986a63254ee2bb30a0929bfff6d6e85b2f2ee290
ottatsuharu <ottatsuharu@gmail.com>
2021-08-24 11:40:30 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get 'contact' => "static_pages#contact"
   get  'signup' => "users#new"
   resources :tweets
-  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- 
+  resources :users
 end
