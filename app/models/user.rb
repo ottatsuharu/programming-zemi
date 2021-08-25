e443eae822940eb42e5d5796b81f6ffa0388ecea
ottatsuharu <ottatsuharu@gmail.com>
2021-08-25 01:27:18 +0000

    validation and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  #データを保存する前にメアドを小文字にする
+  before_save { email.downcase! }
+  #nameは必ず存在し，長さが50字以内
+  validates :name, presence: true, length: { maximum: 50 }
+  #メールアドレスのフォーマットを正規表現を使って指定
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_113829) do
+ActiveRecord::Schema.define(version: 2021_08_24_120007) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_08_14_113829) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
